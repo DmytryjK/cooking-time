14e772283e10762dabdae5a0673a7dda77c8cd09
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-11-08 22:29:56 +0200

    add auto-margin for body, fix come-back link on auth page

--- src/pages/AuthorizationPage/AuthorizationPage.tsx
+++ src/pages/AuthorizationPage/AuthorizationPage.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Authentication from './components/Authentication/Authentication';
 import Logo from '../../assets/icons/logo.svg';
 import './AuthorizationPage.scss';
@@ -10,6 +10,7 @@ type AuthorizationPageProps = {
 
 const AuthorizationPage = (props: AuthorizationPageProps) => {
     const { register, login } = props;
+    const navigate = useNavigate();
     return (
         <section className="authorization">
             <div className="authorization__image" />
@@ -18,7 +19,11 @@ const AuthorizationPage = (props: AuthorizationPageProps) => {
                     <NavLink className="authorization__logo" to="/">
                         <img src={Logo} alt="" />
                     </NavLink>
-                    <NavLink className="authorization__nav-link" to="/">
+                    <NavLink
+                        className="authorization__nav-link"
+                        to="#"
+                        onClick={() => navigate(-1)}
+                    >
                         Назад
                     </NavLink>
                 </nav>
