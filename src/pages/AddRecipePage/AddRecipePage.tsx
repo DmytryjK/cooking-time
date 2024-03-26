402e51fefde20abd38e101b273f16a0734110afa
DmytryjK <dmytryj.kyzyma@gmail.com>
2024-03-26 12:19:47 +0200

    add attention notes when user try to add recipe without auth

--- src/pages/AddRecipePage/AddRecipePage.tsx
+++ src/pages/AddRecipePage/AddRecipePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { m, LazyMotion, domAnimation } from 'framer-motion';
+import { m, LazyMotion, domAnimation, AnimatePresence } from 'framer-motion';
 import RecipesForm from '../../shared-components/RecipesForm/RecipesForm';
 import PopUp from '../../shared-components/PopUp/PopUp';
 import { useAppSelector } from '../../hooks/hooks';
@@ -7,6 +7,12 @@ import './AddRecipePage.scss';
 
 const AddRecipePage = () => {
     const { loadingForm, error } = useAppSelector((state) => state.recipes);
+    const isMessageClosed =
+        sessionStorage.getItem('isMessageClosed') === 'true';
+    const { uid } = useAppSelector((state) => state.authentication.user);
+    const [isUnauthorizedAttention, setIsUnauthorizedAttention] = useState(
+        !uid && !isMessageClosed
+    );
     const [isSuccessPopUpShow, setIsSuccessPopUpShow] = useState(false);
 
     useEffect(() => {
@@ -14,6 +20,12 @@ const AddRecipePage = () => {
             setIsSuccessPopUpShow(true);
         }
     }, [loadingForm]);
+
+    useEffect(() => {
+        if (!uid && !isMessageClosed) {
+            setIsUnauthorizedAttention(true);
+        }
+    }, [uid, isMessageClosed]);
 
     return (
         <>
@@ -41,6 +53,59 @@ const AddRecipePage = () => {
                             Заповніть поля нижче, додайте опис та завантажте
                             фото рецепту
                         </p>
+                        <AnimatePresence>
+                            {isUnauthorizedAttention && (
+                                <m.div
+                                    className="add-recepie__unauthorized-attention"
+                                    key="attention"
+                                    initial={{
+                                        opacity: 0,
+                                        display: 'none',
+                                    }}
+                                    animate={{
+                                        opacity: 1,
+                                        display: 'inline-flex',
+                                        transition: {
+                                            duration: 0.3,
+                                            delay: 0.3,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        transition: {
+                                            duration: 0.3,
+                                            transitionEnd: {
+                                                display: 'none',
+                                            },
+                                        },
+                                    }}
+                                >
+                                    <div className="add-recepie__unauthorized-inner">
+                                        <p>
+                                            Зверніть увагу, що ви додаєте
+                                            рецепт, як не авторизований
+                                            користувач. Ваш рецепт зможуть
+                                            редагувати усі користувачі на сайті.
+                                        </p>
+                                        <button
+                                            className="add-recepie__unauthorized-accept"
+                                            type="button"
+                                            onClick={() => {
+                                                sessionStorage.setItem(
+                                                    'isMessageClosed',
+                                                    true.toString()
+                                                );
+                                                setIsUnauthorizedAttention(
+                                                    false
+                                                );
+                                            }}
+                                        >
+                                            Погоджуюсь
+                                        </button>
+                                    </div>
+                                </m.div>
+                            )}
+                        </AnimatePresence>
                         <RecipesForm id="" method="POST" />
                     </div>
                 </m.section>
